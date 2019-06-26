7fbc8e767e390cb16c22854282659fedba5bba1d
smrbts <samueljustinroberts@gmail.com>
2019-06-26 13:18:49 -0500

    React-navigation added

--- Rover-Rentals/App.js
+++ Rover-Rentals/App.js
@@ -1,19 +1,85 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator, createAppContainer } from 'react-navigation'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+class HomeScreen extends React.Component 
+{
+  render()
+  {
+    return (
+      <View style={styles.container}>
+        <Text>Home Screen Test</Text>
+        <Button
+        title="Go to Details"
+        onPress={() => this.props.navigation.navigate('Details', 
+        {
+          itemId: 86,
+          otherParam: 'test item here'
+        })} 
+        />
+      </View>
+    );
+  }
 }
+
+class DetailsScreen extends React.Component
+{
+  render()
+  {
+    const { navigation } = this.props;
+    const itemId = navigation.getParam('itemId', 'NO-ID');
+    const otherParam = navigation.getParam('otherParam', '460');
+
+    return(
+      <View style={styles.container}>
+        <Text>Details Screen</Text>
+        <Text>itemId: {JSON.stringify(itemId)}</Text>
+        <Text>otherParam: {JSON.stringify(otherParam)}</Text>
+        <Button
+        title="Go to Details...again"
+        onPress={() => this.props.navigation.push('Details', 
+        {
+          itemId: Math.floor(Math.random() * 100),
+        })} 
+        />
+         <Button
+          title="Go to Home"
+          onPress={() => this.props.navigation.navigate('Home')}
+        />
+        <Button
+          title="Go back"
+          onPress={() => this.props.navigation.goBack()}
+        />
+      </View>
+    )
+  }
+}
+
+const RootStack = createStackNavigator(
+  {
+    Home: {screen: HomeScreen},
+    Details: {screen: DetailsScreen},
+  },
+  {
+    initialRouteName: 'Home',
+  }
+  )
+
+const AppContainer = createAppContainer(RootStack)
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#2D5363',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
+export default class App extends React.Component
+{
+  render()
+  {
+    return <AppContainer />
+  }
+}
