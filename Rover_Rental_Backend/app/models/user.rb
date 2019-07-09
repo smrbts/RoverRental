3d6dc1b36629a46622b1cc7866b037aafcc93212
smrbts <samueljustinroberts@gmail.com>
2019-07-09 09:01:26 -0500

    before merge

--- Rover_Rental_Backend/app/models/user.rb
+++ Rover_Rental_Backend/app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
     
+    validates :username, uniqueness: {case_sensitive: false}
     has_many :walks
     has_many :dogs, through: :walks
 end
